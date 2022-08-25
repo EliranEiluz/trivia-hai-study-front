dd7b17e7e5fa987429552f7f50eb2d4ed8b13c52
EliranEiluz <eliranfr1@gmail.com>
2022-08-25 13:12:59 +0300

    changes

--- src/GamePage.js
+++ src/GamePage.js
@@ -1,7 +1,7 @@
 import './GamePage.css';
 import { useEffect, useRef, useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
-import $ from 'jquery';
+
 function GamePage({ nowOnline }) {
 
 
